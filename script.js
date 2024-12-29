ee2ef2203040ba3dd70da0966aa362c665d003c5
webdev-raj <codewithsapnap@gmail.com>
2024-12-29 21:52:40 +0530

    miniaml change done

---
+++ script.js
@@ -0,0 +1,10 @@
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
