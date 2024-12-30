d5dbd767b07c972668330193a6723f2b1db6897c
webdev-raj <codewithsapnap@gmail.com>
2024-12-30 12:13:05 +0530

    done

--- script.js
+++ script.js
@@ -10,3 +10,54 @@ requestAnimationFrame(raf);
 }
 scrollAnimation()
 
+var tl = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#mainIceCream",
+    scroller:"body",
+    markers:true,
+    start:"20% 10%",
+    end:"90% 10%",
+    scrub:2,
+  }
+})
+
+tl.to("#mainIceCream",{
+  y:940,
+  rotate:0,
+  duration:2,
+  height:"70vh"
+})
+
+var tl2 = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#page2",
+    scroller:"body",
+    markers:true,
+    start:"20% 10%",
+    end:"90% 10%",
+    scrub:2,
+  }
+})
+
+tl2.to("#mainIceCream",{
+  y:1710,
+  x:-410,
+  height:"50vh"
+})
+
+var tl3 = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#page3",
+    scroller:"body",
+    markers:true,
+    start:"20% 10%",
+    end:"90% 10%",
+    scrub:2,
+  }
+})
+
+tl3.to("#mainIceCream",{
+  y:2210,
+  rotate:"-50deg",
+  height:"65vh"
+})
